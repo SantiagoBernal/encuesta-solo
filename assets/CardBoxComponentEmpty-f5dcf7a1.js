51098ab217301b677557a7bec6619a6b45279d00
JEFE DE DESARROLLO <jdesarrollo@JEFEDESARROLLO.commerk.local>
2024-03-18 21:28:05 -0500

    Updates

---
+++ assets/CardBoxComponentEmpty-f5dcf7a1.js
@@ -0,0 +1,1 @@
+import{Z as e,o as t,f as o,g as s}from"./index-354d724b.js";const c={},n={class:"text-center py-24 text-gray-500 dark:text-slate-400"},r=s("p",null,"Nothing's here…",-1),a=[r];function _(d,i){return t(),o("div",n,a)}const l=e(c,[["render",_]]);export{l as C};
